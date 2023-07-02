4bc456785e100289d5839ea334a3bdcb505908aa
Tomas-Jankauskas <tomas.jankauskas@hostinger.com>
2023-07-02 22:53:13 +0900

    invalid password error message

--- src/types/enums/errorEnums.ts
+++ src/types/enums/errorEnums.ts
@@ -1,4 +1,5 @@
 export const Errors = {
   ['auth/user-not-found']: 'Invalid username or password',
+  ['auth/wrong-password']: 'Invalid password',
   ['auth/email-already-in-use']: 'Email already in use'
 };
